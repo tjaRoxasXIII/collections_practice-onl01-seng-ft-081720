fe766c72dac84fa1dabbb9eb41cbdbdfc2d25de7
Taylor Allen <tjaroguetech@gmail.com>
2020-08-19 20:30:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,5 +11,8 @@ def sort_array_char_count(array)
 end 
 
 def swap_elements(array)
-  array[1], array[2] = array[2], array[1]
+  new_array = array
+  new_array[1] = array[2]
+  new_array[2] = array[1]
+  return new_array
 end
